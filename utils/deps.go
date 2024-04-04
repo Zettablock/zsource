fdba9bde1c3e3f1c493148f5bc9c1b8eaefbda2c
Chengcheng Pei <chengchengpei@outlook.com>
2024-04-04 14:52:06 -0700

    update

--- utils/deps.go
+++ utils/deps.go
@@ -1,14 +1,13 @@
 package utils
 
 import (
-	"github.com/Zettablock/demo2/model"
 	"golang.org/x/exp/slog"
 	"gorm.io/gorm"
 	"plugin"
 )
 
 type Deps struct {
-	StatsDao  *model.DemoBlockStatsDao
+	StatsDao  *dao.DemoBlockStatsDao
 	SourceDBs map[string]*gorm.DB
 	DestDBs   map[string]*gorm.DB
 	Logger    *slog.Logger
