0976f413979489e9e24b157fc244b8f8c66b3297
mwrrlvkixj <feiran.yang@zettablock.com>
2024-06-07 17:59:55 +0800

    add support for beacon

--- utils/deps.go
+++ utils/deps.go
@@ -1,14 +1,16 @@
 package utils
 
 import (
+	"plugin"
+
 	"github.com/Zettablock/zsource/configs"
 	"golang.org/x/exp/slog"
 	"gorm.io/gorm"
-	"plugin"
 )
 
 type Deps struct {
 	SourceDB            *gorm.DB
+	SourceDBSchema      string
 	DestinationDB       *gorm.DB
 	DestinationDBSchema string
 	MetadataDB          *gorm.DB
